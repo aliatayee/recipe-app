5f151c8eaf6bd17909d4372b61ced640c0c6ac95
aliatayee <aliaqaatayee@gmail.com>
2022-10-03 17:35:38 +0430

    Add migrations for food & user

---
+++ db/migrate/20221003130100_create_food.rb
@@ -0,0 +1,12 @@
+class CreateFood < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+
+      t.string :name
+      t.integer :measurement_unit 
+      t.double :price 
+      t.double :quantity
+      t.timestamps
+    end
+  end
+end
