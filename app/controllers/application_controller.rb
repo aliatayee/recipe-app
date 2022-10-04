bb5ff0bd417d3464a07fb1455dbea0da29c34ed6
aliatayee <aliaqaatayee@gmail.com>
2022-10-04 15:18:51 +0430

    Fix linter issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   # prevent users to perfrom action without authentication
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
