0d845c9e7931c2cd5e43ec686730a21aa6caf664
aliatayee <aliaqaatayee@gmail.com>
2022-10-06 11:12:26 +0430

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,10 +10,12 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :email, :password, :current_password)
     end
   end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
   end
+
   def after_sign_out_path_for(_scope)
     root_path
   end
--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
-    def index
-        @users = User.all
-        @recipes = Recipe.includes([:user]).where(public: true)
-    end
+  def index
+    @users = User.all
+    @recipes = Recipe.includes([:user]).where(public: true)
+  end
 end
