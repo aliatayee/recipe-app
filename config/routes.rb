8d433014163fddaf7ac1315e68c969d9782e8a00
aliatayee <aliaqaatayee@gmail.com>
2022-10-06 11:11:43 +0430

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :users
+  get "/public_recipes", to: "recipes#public_recipes"
+  resources :users do
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipe_foods, only: %i[create destroy]
   end
-  resources :foods, only: %i[index show new create destroy]
-  resources :foods, only: %i[index new create destroy] do
+  resources :foods, only: %i[index show new create destroy] do
     resources :recipe_foods, only: %i[create destroy]
+    end
   end
 end
