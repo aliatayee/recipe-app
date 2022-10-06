8918adcca9b8a0b1b1e63a8992359c3c2f75d543
aliatayee <aliaqaatayee@gmail.com>
2022-10-06 21:49:37 +0430

    Add food views and functionality

--- db/migrate/20221003130814_create_recipe_foods.rb
+++ db/migrate/20221003130814_create_recipe_foods.rb
@@ -1,4 +1,4 @@
-class CreateRecipeFood < ActiveRecord::Migration[7.0]
+class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
       t.references :food, null: false, index: true, foreign_key: true
