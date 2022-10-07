67c6f8fb6904ba2dfbcf09acc0cbe68fb549d09d
aliatayee <aliaqaatayee@gmail.com>
2022-10-07 11:51:31 +0430

    Add shopping list controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   get "/public_recipes", to: "recipes#public_recipes"
+  get '/shopping_list', to: 'shopping_list#index', as: 'shopping_list'
   resources :users do
   resources :recipes, only: %i[index show new create destroy] do
     resources :recipe_foods, only: %i[create destroy]
